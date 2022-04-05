ecc1ec38791002f387067e9cba6a52e7ffd0d330
Дмитрий <vasilenko.dmitrii@phystech.edu>
2022-04-05 16:54:44 +0300

    fixed parse_planets_parameters and parse_star_parameters

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +47,14 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    star.m = line.split()[3]
+    star.x = line.split()[4]
+    star.y = line.split()[5]
+    star.Vx = line.split()[6]
+    star.Vy = line.split()[7]
+    star.R = line.split()[1]
+    star.color = line.split()[2]
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +71,13 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet.m = line.split()[3]
+    planet.x = line.split()[4]
+    planet.y = line.split()[5]
+    planet.Vx = line.split()[6]
+    planet.Vy = line.split()[7]
+    planet.R = line.split()[1]
+    planet.color = line.split()[2]
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -79,6 +95,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME: should store real values
+
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
