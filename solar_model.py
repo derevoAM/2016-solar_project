1a93c40239da0ad5eab3034c8ee531ee01328194
Дмитрий <vasilenko.dmitrii@phystech.edu>
2022-04-05 18:01:45 +0300

    fixed calculate_force

--- solar_model.py
+++ solar_model.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # license: GPLv3
 
-gravitational_constant = 6.67408E-11
+GRAVITATIONAL_CONSTANT = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
 
@@ -18,9 +18,8 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += GRAVITATIONAL_CONSTANT * body.m * obj.m * (body.x - obj.x)/((abs(body.x - obj.x)))**3
+        body.Fy += GRAVITATIONAL_CONSTANT * body.m * obj.m * (body.y - obj.y)/((abs(body.y - obj.y)))**3
 
 
 def move_space_object(body, dt):
