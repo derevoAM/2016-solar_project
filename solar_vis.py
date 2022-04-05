0762bc0ea740be37a6472b66888ccaee5a9f78a4
derevoAM <derevianchenko.ma@phystech.edu>
2022-04-05 16:40:57 +0300

    Team_lead stage one complete

--- solar_vis.py
+++ solar_vis.py
@@ -84,7 +84,6 @@ def create_planet_image(space, planet):
     y = scale_y(planet.y)
     r = planet.R
     planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
-    pass  # FIXME: сделать как у звезды
 
 
 def update_system_name(space, system_name):
