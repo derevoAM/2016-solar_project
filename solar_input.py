94e8c0ac47c0748033fb6b20f0e6135b18895823
derevoAM <derevianchenko.ma@phystech.edu>
2022-04-12 04:56:43 +0300

    Format of writing to file was changed

--- solar_input.py
+++ solar_input.py
@@ -94,8 +94,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             out_file.writelines(
-                ("{} {} {} {} {} {} {} {}".format(obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.vx,
-                                                  obj.vy)).title())
+                ("{} {} {} {} {} {} {} {} \n".format(obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx,
+                                                  obj.Vy)).title())
 
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
